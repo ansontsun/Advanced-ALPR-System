ab11a992bd8ffb86af5069f9fc90b7db842043ee
Edward Liu <69edwardliu@gmail.com>
2024-06-15 00:39:51 +0800

    Update git ignore files

--- yolov5-env/Scripts/activate.bat
+++ yolov5-env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\69edw\Desktop\Advanced-ALPR-System\yolov5-env
+set VIRTUAL_ENV=E:\APS360\Advanced-ALPR-System\yolov5-env
 
 if not defined PROMPT set PROMPT=$P$G
 
