018a2a112a286a38382a67723d05e814138b0477
Dawae111 <DavidApply2022@outlook.com>
2024-06-21 00:28:27 +0800

    fixed several mistakes

--- Scripts/train_yolov10.py
+++ Scripts/train_yolov10.py
@@ -1,18 +1,14 @@
-import torch
-from ultralytics import YOLOv10
-# import os
-# os.environ["HF_ENDPOINT"]= "https://hf-mirror.com"
 
-# model = YOLOv10()
-# model_path = r'E:\APS360\Advanced-ALPR-System\yolov10n.pt'
-# model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+
+from ultralytics import YOLOv10
+import torch
 
 
 #model.push_to_hub("your-hf-username/yolov10-finetuned")
 def main():
     model = YOLOv10.from_pretrained('jameslahm/yolov10n')
     model.train(data='data/data.yaml', epochs=50, batch=10, imgsz=640)
-    model.val(data='data/data.yaml', batch=256)
+    model.val(data='data/data.yaml', batch=10)
 
 
 if __name__ == '__main__':
