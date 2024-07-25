e5ed7921700c577210fde15a4447110cebcf409e
Dawae111 <DavidApply2022@outlook.com>
2024-07-26 00:30:13 +0800

    deleted runs in incorrectfolder

---
+++ Scripts/train/train_new.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+
+def main():
+    model = YOLO("yolov10s.pt")
+    #model = YOLOv10.from_pretrained('jameslahm/yolov10n')
+    results = model.train(data='data/data.yaml', epochs=20, batch=16, imgsz=640)
+    #model.val(data='data/data.yaml', batch=16)
+
+    #results = model.predict(0, save = True, show = True, conf = 0.15)
+
+
+if __name__ == '__main__':
+    import multiprocessing
+    multiprocessing.freeze_support()
+    main()
