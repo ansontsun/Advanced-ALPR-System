7b9fbddd066b9f0a7e4a7cc345603c2544a816f2
Dawae111 <DavidApply2022@outlook.com>
2024-08-03 18:14:02 +0800

    added l model m model for tuning

--- Scripts/train/train_new.py
+++ Scripts/train/train_new.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 def main():
     model = YOLO("yolov10m.pt")
     #model = YOLOv10.from_pretrained('jameslahm/yolov10n')
-    results = model.train(data='data/data.yaml', epochs=200, batch=8, imgsz=640)
+    results = model.train(data='data/Roboflow dataset/data.yaml', epochs=100, batch=8, imgsz=640)
     #model.val(data='data/data.yaml', batch=16)
     #results = model.predict(0, save = True, show = True, conf = 0.15)
 if __name__ == '__main__':
